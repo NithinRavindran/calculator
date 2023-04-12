47aaaf04d1b855c1ada526bc0f66d1170117ed9c
Nithin <nithinravindran1990@gmail.com>
2023-04-13 02:13:45 +0530

    update

--- calculator.js
+++ calculator.js
@@ -6,11 +6,14 @@ function allclear() {
     result.value = "";
 }
 
-function evaluate() {
-    resut.value=eval(result.value)
-}
-
 function del() {
     s = result.value;
     result.value = s.slice(0, -1);
+}
+function evaluateexpr() {
+    result.value = eval(result.value);
+}
+
+function sqrroot(n) {
+    result.value=math.sqrt(n)
 }
